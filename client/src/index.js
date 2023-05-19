59061629fa60b54f5f017c9d3728495a89f1e7e6
Rijumone Choudhuri <mailmeonriju@gmail.com>
2023-05-19 09:13:32 +0530

    MUI and loading... message

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
+
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Tooltip from '@mui/material/Tooltip'
+import TableContainer from '@mui/material/TableContainer'
+import Table from '@mui/material/Table'
+import TableHead from '@mui/material/TableHead'
+import TableCell from '@mui/material/TableCell'
+import TableRow from '@mui/material/TableRow'
+import TableBody from '@mui/material/TableBody'
+
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
@@ -19,46 +30,57 @@ class SearchBox extends React.Component {
     super(props);
     this.state = {
       data: null,
+      loading: false,
     };
   }
   handleSubmit = (event) => {
     event.preventDefault();
+    this.setState({ ...this.state.data, loading: true }) // spread? Like python **args
     console.log(this.input.value);
     fetch("http://albumart-gen.hopto.org:5005?searchterm=" + this.input.value, {
     // fetch("http://localhost:5000?searchterm=" + this.input.value, {
       method: "GET",
       // body: formData
     })
-      .then((response) => response.json())
-      .then((data) => this.setState({ data }));
+      .then((response) => response.json()) // (response) => response.json() this is a function, will pass the value to the next then()
+      .then((data) => this.setState({ data, loading: false }));
   };
   render() {
     return (
-      <>
+      <center>
         <form onSubmit={this.handleSubmit}>
-          <label>
-            Search:
+          <Tooltip title="Enter fully or in part" arrow placement="left">
+            {/* <TextField variant="standard" label="Title or author" ref={(input) => (this.input = input)} /> */}
             <input type="text" ref={(input) => (this.input = input)} />
-          </label>
-          <button type="submit">Submit</button>
+          </Tooltip>
+          <Button variant="contained" type="submit">Search</Button>
+          <span>{ this.state.loading ? 'loading...' : ''}</span>
         </form>
         {this.state.data && (
-          <table>
-            <tbody>
-              {this.state.data.map((book) => (
-                <tr key={book.title}>
-                  <td>{book.title}</td>
-                  <td>
-                    <a href={book.mirror_list} rel="noreferrer" target="_blank">
-                      {book.mirror_list}
-                    </a>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <TableContainer
+          // component={Paper} 
+          variant="outlined"
+          >
+              <Table aria-label="demo table">
+              <TableHead><TableRow>
+              <TableCell>Title</TableCell><TableCell>Download link</TableCell>
+              </TableRow></TableHead>
+              <TableBody>
+                {this.state.data.map((book) => (
+                  <TableRow key={book.title}>
+                    <TableCell>{book.title}</TableCell>
+                    <TableCell>
+                      <a href={book.mirror_list} rel="noreferrer" target="_blank">
+                        {book.mirror_list}
+                      </a>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         )}
-      </>
+      </center>
     );
   }
 }
